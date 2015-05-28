f076eb63c5a8f05d3eb20ca886dc8e166e46093f
Rp7rf <is0248vx@ed.ritsumei.ac.jp>
2015-05-28 09:29:41 +0900

    add memo

--- js/weekday.js
+++ js/weekday.js
@@ -1,4 +1,5 @@
 $(function(){
 	now  = new Date();
+//	$("#week td:nth-child("+(now.getDay()+1)+")").removeClass().addClass("hover");
 	$("#week td:nth-child("+(now.getDay()+1)+")").addClass("hover");
 })
