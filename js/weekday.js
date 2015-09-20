b129391c3ac564d1e2c1a09730310e7201492469
Koichi Uchinishi <is0248vx@ed.ritsumei.ac.jp>
2015-09-20 10:53:42 +0900

    wq  Please enter the commit message for your changes. Lines starting

--- js/weekday.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-	now  = new Date();
-//	$("#week td:nth-child("+(now.getDay()+1)+")").removeClass().addClass("hover");
-	$("#week td:nth-child("+(now.getDay()+1)+")").addClass("hover");
-})
